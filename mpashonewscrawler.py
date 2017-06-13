30d84ce336a7a69857e1bc0fcb43a2f0d1cfd149
DavidMwangi <david.mathenge98@gmail.com>
2017-06-13 17:01:37 +0300

    mpasho site news crawler preliminary. Extracts categories, rss feeds and news articles urls

---
+++ mpashonewscrawler.py
@@ -0,0 +1,38 @@
+import newspaper
+
+dailynews = newspaper.build('https://mpasho.co.ke/', memoize_articles=False)
+#memoize_articles=False parameter opts out of default cache elimination
+
+print(dailynews.brand.capitalize() + ': ' + dailynews.description)
+
+
+#CACHING ARTICLE NUMBER
+
+print('\nDaily Nation Cached Articles\n')
+
+print(dailynews.size())
+
+
+#EXTRACTING ARTICLE URLS
+
+
+for article in dailynews.articles:
+
+    print(article.url)
+
+
+#EXTRACTING SOURCE CATEGORIES
+
+print('\nDaily Nation Categories\n')
+
+for category in dailynews.category_urls():
+
+    print(category)
+
+#EXTRACTING SOURCE FEEDS
+
+print('\nDaily Nation RSS Feeds\n')
+
+for feed_url in dailynews.feed_urls():
+
+    print (feed_url)
