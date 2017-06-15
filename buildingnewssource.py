281a38e055e5b8795d14183d71f8531815592cbd
DavidMwangi <david.mathenge98@gmail.com>
2017-06-15 16:42:49 +0300

    News title test worked

--- buildingnewssource.py
+++ buildingnewssource.py
@@ -1,12 +1,36 @@
 import newspaper
 
-cnn_paper = newspaper.build('http://www.nation.co.ke/')
 
-"""
-for article in cnn_paper.articles:
+mpashonews = newspaper.build('https://mpasho.co.ke/', memoize_articles=False)
+#memoize_articles=False parameter opts out of default cache elimination
 
-    print(article.url)
-"""
+print(mpashonews.brand.capitalize() + ': ' + mpashonews.description)
 
 
-print(cnn_paper.size())
+#CACHING ARTICLE NUMBER
+
+print('\nMpasho Cached Articles\n')
+
+print(mpashonews.size())
+
+articles = list()
+
+
+for category in mpashonews.category_urls():
+
+    print(category)
+
+#EXTRACTING ARTICLE URLS
+
+
+for article in mpashonews.articles:
+
+    article.download()
+
+    article.parse()
+
+    print(article.title + '\n')
+
+
+    
+#print(articles)
